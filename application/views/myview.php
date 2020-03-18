4fdf9a870bfbc5885ca236861b890fe5e05ec7d4
aliffrhn <alif.farhanw@gmail.com>
2020-03-18 23:00:54 +0700

    change autoload url, add bootstrap in assets, set assets

--- application/views/myview.php
+++ application/views/myview.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test Pass Array</title>
+    <link rel="stylesheet" href="<?php echo base_url().'assets/css/bootstrap.min.css'?>" />
 </head>
 <body>
     File View Pertama
